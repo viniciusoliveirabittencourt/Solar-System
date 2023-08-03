3076409335e2be76010f6ddfb524bd3212f23f73
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2023-08-03 11:04:18 -0300

    adicionando Missions ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './sections/Header/Header'
 import Planets from './sections/Planets/Planets'
 import './App.css'
+import Missions from './sections/Missions/Missions'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Planets />
+      <Missions />
     </>
   )
 }
